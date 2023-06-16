f61e271a006de2063bf6e10ecfea60dd70461d9a
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2023-06-16 11:31:59 -0300

    added basic template

--- treelinks/urls.py
+++ treelinks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.conf.urls import include
+from treelinks.views.home import HomePageView
 
 
 urlpatterns = [
-
+    path("", HomePageView.as_view(), name="home"),
 ]
